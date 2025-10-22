b75c84f64537dcac8032fbc015e5d1eaf7fcb3de
LUIS_LAGUNES <LUIS_LAGUNES@gmail.com>
2025-10-21 20:49:58 -0500

    Start of flask app

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request  # NOT the same as requests 
+from gemini import get_songs
+
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return render_template('index.html')
+
+
+@app.route('/get_mood')
+def get_user_info():
+    # get gemini from gemini api and display on new page
+    mood = request.args.get('Mood')
+    songs = get_songs(mood)
+    return render_template('gemini.html', user_mood=mood, user_songs=songs)
+
+if __name__ == '__main__':
+    app.run()
