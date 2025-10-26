cf7f1c9b73cfe0c1e9ab7c320cc84eac96fd4e00
r.qui <r.qui@gmail.com>
2025-10-26 12:44:54 -0500

    Issue #3: Add initial project setup files and configurations reverting changes in app.py and gemini.py based con PR comments.

--- gemini.py
+++ gemini.py
@@ -1,15 +1,14 @@
 from google import genai
-import os 
+import os
 
 GOOGLE_API_KEY = os.environ.get('GEMINI_API_KEY')
 client = genai.Client(api_key=GOOGLE_API_KEY)
 
 def get_songs(mood):
     response = client.models.generate_content(
-    model='gemini-2.5-flash', 
+    model='gemini-2.5-flash',
     contents=f"""You are Moodify, you will recommend 5 songs based on the users mood, if the user specifies a genre,
     then do 5 songs within that genre, if the user does not specify the genre then suggest 5 songs of a random genre based on the users mood. : {mood}"""
 )
     return response.text
-
 
